286d10cae0e7f08cd26e0b30ffd5786a6d6588a1
Dolce Tentazione Sin Tacc <96395905+DulcesTentacionessintacc@users.noreply.github.com>
2024-12-04 00:49:50 -0300

    Add files via upload

--- js/form.js
+++ js/form.js
@@ -9,35 +9,100 @@ function precio(){
 let listacremas = document.querySelector("#listacremas").value;
    
   
-if(listacremas == "TORTA FRUTAL"){
-  document.getElementById("salida").innerHTML = "$25000";
-  document.getElementById("des1").innerHTML = "BIZCOCHUELO DE VAINILLA<br><br>DULCE DE LECHE REPOSTERO<br><br>CREMA DE LECHE<br><br>FRUTAS (FRUTILLA - KIWI - DURAZNO)";
-  imagenTorta.innerHTML = '<img src="frutal.jpg" alt="Torta Rogel" class="imgTorta" />';
+if(listacremas == "PAN CHIPS - CHICO"){
+  document.getElementById("salida").innerHTML = "$9800";
+  document.getElementById("des1").innerHTML = "CHIPS DE CHOCOLATE<br><br>Peso aprox. 450 gr.<br>";
+  imagenTorta.innerHTML = '<img src="panchips.jpg" alt="Torta Rogel" class="imgTorta" />';
 }
-else if (listacremas == "BOX DESAYUNO"){
-  document.getElementById("salida").innerHTML = "$30000";
-  document.getElementById("des1").innerHTML = "1 MEDIALUNA DE MANTECA<br><br>1 VIGILANTE<br><br>1 MAICENA<br><br>1 PORCIÓN DE PASTAFROLA<br><br>1 ALFAJOR DE CHOCOLATE NEGRO<br><br>1 ALFAJOR DE CHOCOLATE BLANCO<br><br>2 SÁNGUCHE LA DE MIGA<br><br>2 PANES SABORIZADOS DE JAMÓN Y QUESO<br><br>SEMOLADAS<br><br>1 CAFÉ<br><br>1 CAPUCHINO<br><br>1 TAZA<br><br>1 RAMO DE FLORES<br><br>1 TARJETA ALUSIVA";
-  imagenTorta.innerHTML = '<img src="box.jpg" alt="Torta Rogel" class="imgTorta" />';
+else if (listacremas == "PAN CHIPS - GRANDE"){
+  document.getElementById("salida").innerHTML = "$18500";
+  document.getElementById("des1").innerHTML = "CHIPS DE CHOCOLATE<br><br>Peso aprox. 750/850 gr.<br>";
+  imagenTorta.innerHTML = '<img src="panchips.jpg" alt="Torta Rogel" class="imgTorta" />';
 }
-else if (listacremas == "BENTO CAKE + CUPCAKES"){
-  document.getElementById("salida").innerHTML = "$23500";
-  document.getElementById("des1").innerHTML = "1 BENTO CAKE DE VAINILLA RELLENO CON DULCE DE LECHE REPOSTERO Y CREMA DE LECHE<br><br>2 CUPCAKES DE VAINILLA - AMBOS DECORADOS CON BUTTERCREAM (COLOR A ELECCIÓN)<br><br>TARJETA ALUSIVA<br><br>RAMITO DE FLOR";
- imagenTorta.innerHTML = '<img src="bento.jpg" alt="Torta Rogel" class="imgTorta" />';
+else if (listacremas == "PAN RELLENO - CHICO"){
+  document.getElementById("salida").innerHTML = "$9500";
+  document.getElementById("des1").innerHTML = "RELLENO DE CREMA PASTELERA Y DULCE DE LECHE<br><br>Peso aprox. 450 gr.";
+ imagenTorta.innerHTML = '<img src="panrelleno.jpg" alt="Torta Rogel" class="imgTorta" />';
 }
-else if (listacremas == "BOMBAS DE CREMAS"){
+else if (listacremas == "PAN RELLENO - GRANDE"){
+  document.getElementById("salida").innerHTML = "$18000";
+  document.getElementById("des1").innerHTML = "RELLENO DE CREMA PASTELERA Y DULCE DE LECHE<br><br>Peso aprox. 750/850 gr.";
+ imagenTorta.innerHTML = '<img src="panrelleno.jpg" alt="Torta Rogel" class="imgTorta" />';
+}
+else if (listacremas == "PAN FIT - CHICO"){
+  document.getElementById("salida").innerHTML = "$14000";
+  document.getElementById("des1").innerHTML = "SIN AZUCAR -  PROTEICO - APTO DIABETICOS<br><br>Peso aprox. 300/400 gr.";
+ imagenTorta.innerHTML = '<img src="panfit.jpg" alt="Torta Rogel" class="imgTorta" />';
+}
+else if (listacremas == "PAN FIT - GRANDE"){
+  document.getElementById("salida").innerHTML = "$26200";
+  document.getElementById("des1").innerHTML = "SIN AZUCAR -  PROTEICO - APTO DIABETICOS<br><br>Peso aprox. 500/600 gr.";
+ imagenTorta.innerHTML = '<img src="panfit.jpg" alt="Torta Rogel" class="imgTorta" />';
+}
+else if (listacremas == "PANETONNE - CHICO"){
+  document.getElementById("salida").innerHTML = "$16500";
+  document.getElementById("des1").innerHTML = "- LINEA PREMIUM -<br><br>MIX DE FRUTOS SECOS Y GLASE";
+ imagenTorta.innerHTML = '<img src="panetone.jpg" alt="Torta Rogel" class="imgTorta" />';
+}
+else if (listacremas == "PANETONNE - GRANDE"){
+  document.getElementById("salida").innerHTML = "$31500";
+  document.getElementById("des1").innerHTML = "- LINEA PREMIUM -<br><br>MIX DE FRUTOS SECOS Y GLASE";
+ imagenTorta.innerHTML = '<img src="panetone.jpg" alt="Torta Rogel" class="imgTorta" />';
+}
+else if (listacremas == "BUDIN INGLES"){
+  document.getElementById("salida").innerHTML = "$11900";
+  document.getElementById("des1").innerHTML = "Peso aprox. 300 gr.";
+ imagenTorta.innerHTML = '<img src="budini.jpg" alt="Torta Rogel" class="imgTorta" />';
+}
+else if (listacremas == "BUDIN RELLENO"){
+  document.getElementById("salida").innerHTML = "$10000";
+  document.getElementById("des1").innerHTML = "RELLENO DE DULCE DE LECHE<br><br>Peso aprox. 300 gr.";
+ imagenTorta.innerHTML = '<img src="budinr.jpg" alt="Torta Rogel" class="imgTorta" />';
+}
+else if (listacremas == "PACK N°1 - A"){
+  document.getElementById("salida").innerHTML = "$26900";
+  document.getElementById("des1").innerHTML = "PAN RELLENO GRANDE + BUDIN INGLES";
+ imagenTorta.innerHTML = '<img src="" alt="" class="imgTorta" />';
+}
+else if (listacremas == "PACK N°1 - B"){
+  document.getElementById("salida").innerHTML = "$25250";
+  document.getElementById("des1").innerHTML = "PAN RELLENO GRANDE + BUDIN RELLENO";
+ imagenTorta.innerHTML = '<img src="" alt="" class="imgTorta" />';
+}
+else if (listacremas == "PACK N°2 - A"){
+  document.getElementById("salida").innerHTML = "$27150";
+  document.getElementById("des1").innerHTML = "PAN CHIPS GRANDE + BUDIN INGLES";
+ imagenTorta.innerHTML = '<img src="" alt="" class="imgTorta" />';
+}
+else if (listacremas == "PACK N°2 - B"){
+  document.getElementById("salida").innerHTML = "$25500";
+  document.getElementById("des1").innerHTML = "PAN CHIPS GRANDE + BUDIN RELLENO";
+ imagenTorta.innerHTML = '<img src="" alt="" class="imgTorta" />';
+}
+else if (listacremas == "TRONCO DE ARBOL"){
+  document.getElementById("salida").innerHTML = "$20000";
+  document.getElementById("des1").innerHTML = "RINDE 10 PORCIONES";
+ imagenTorta.innerHTML = '<img src="tronco.jpg" alt="Torta Rogel" class="imgTorta" />';
+}
+else if (listacremas == "BOMBAS CREMAS X 12"){
   document.getElementById("salida").innerHTML = "$10000";
   document.getElementById("des1").innerHTML = "12 UNIDADES";
- imagenTorta.innerHTML = '<img src="bombas.jpg" alt="Torta Rogel" class="imgTorta" />';
+ imagenTorta.innerHTML = '<img src="bombasx.jpg" alt="Torta Rogel" class="imgTorta" />';
 }
-else if (listacremas == "BOMBAS MIXTAS"){
-  document.getElementById("salida").innerHTML = "$11500";
+else if (listacremas == "BOMBAS MIXTAS X 12"){
+  document.getElementById("salida").innerHTML = "$11000";
   document.getElementById("des1").innerHTML = "12 UNIDADES";
- imagenTorta.innerHTML = '<img src="bombas.jpg" alt="Torta Rogel" class="imgTorta" />';
+ imagenTorta.innerHTML = '<img src="bombasx.jpg" alt="Torta Rogel" class="imgTorta" />';
 }
-else if (listacremas == "SANDWICH DE MIGA"){
-  document.getElementById("salida").innerHTML = "$18750";
-  document.getElementById("des1").innerHTML = "12 UNIDADES";
- imagenTorta.innerHTML = '<img src="sand.jpg" alt="Torta Rogel" class="imgTorta" />';
+else if (listacremas == "COMBO NAVIDAD DOLCE - N°1"){
+  document.getElementById("salida").innerHTML = "$30000";
+  document.getElementById("des1").innerHTML = "MIGAS POR 6 UNIDADES<br><br>1 PAN CHICO A ELECCION (Chips o Relleno)<br><br> 1 BUDIN A ELECCION (Ingles o Relleno)";
+ imagenTorta.innerHTML = '<img src="combonav.jpg" alt="Torta Rogel" class="imgTorta" />';
+}
+else if (listacremas == "COMBO NAVIDAD DOLCE - N°2"){
+  document.getElementById("salida").innerHTML = "$57000";
+  document.getElementById("des1").innerHTML = "MIGAS POR 12 UNIDADES<br><br> 1 PANETONNE CHICO<br><br>1 PAN CHICO A ELECCION(Chips  o Relleno)<br><br>1 BUDIN A ELECCION (Ingles o Relleno)";
+ imagenTorta.innerHTML = '<img src="combonav.jpg" alt="Torta Rogel" class="imgTorta" />';
 }
 else{}
 }
